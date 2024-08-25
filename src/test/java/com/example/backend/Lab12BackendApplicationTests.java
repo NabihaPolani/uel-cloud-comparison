799af3b68cb0ac6c7acd3406face74683fb2c1f6
Muhammad Zohaib Baig <xubii@Muhammads-MacBook-Pro.local>
2024-08-25 16:44:33 +0100

    remove the hardcord credentials

--- src/test/java/com/example/backend/Lab12BackendApplicationTests.java
+++ src/test/java/com/example/backend/Lab12BackendApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Lab12BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class Lab12BackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
